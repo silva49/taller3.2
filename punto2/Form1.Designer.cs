ce39c4c7f57ddb9d592cf0748c47f5b62267c7e0
cristian david tilano silva <cristiantilanosilva@gmail.com>
2019-09-01 13:00:38 -0500

    punto dos del taller3, salario

---
+++ punto2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace punto2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txthoras = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtvalor = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculador de salarios de empleados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese sus horas trabajadas: \r\n";
+            // 
+            // txthoras
+            // 
+            this.txthoras.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txthoras.Location = new System.Drawing.Point(340, 154);
+            this.txthoras.Name = "txthoras";
+            this.txthoras.Size = new System.Drawing.Size(156, 35);
+            this.txthoras.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(303, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ingrese el valor de cada hora: ";
+            // 
+            // txtvalor
+            // 
+            this.txtvalor.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtvalor.Location = new System.Drawing.Point(340, 228);
+            this.txtvalor.Name = "txtvalor";
+            this.txtvalor.Size = new System.Drawing.Size(156, 35);
+            this.txtvalor.TabIndex = 4;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.SystemColors.Info;
+            this.btncalcular.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(539, 174);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(160, 61);
+            this.btncalcular.TabIndex = 5;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(57, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 33);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "TOTAL:";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbltotal.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotal.Location = new System.Drawing.Point(182, 349);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(152, 29);
+            this.lbltotal.TabIndex = 7;
+            this.lbltotal.Text = "SalarioAPagar";
+            this.lbltotal.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtvalor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txthoras);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txthoras;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtvalor;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbltotal;
+    }
+}
+
---
+++ punto2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace punto2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double horastrabajadas = double.Parse(txthoras.Text);
+            double valorhora = double.Parse(txtvalor.Text);
+            double total = horastrabajadas * valorhora;
+
+            if (total>750000) {
+                MessageBox.Show("Tú salario es mayor al mínimo");
+                
+            }
+
+            else {
+                total = (total * 0.20)+ total;
+                
+
+
+            }
+            lbltotal.Text = "$"+ Convert.ToDecimal(total).ToString("N0");
+            lbltotal.Visible = true;
+        }
+    }
+}
